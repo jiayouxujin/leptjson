2feaa450049e4f63dd47c6e26aecb5f2371e7a29
xuxiaojin <jyxuxiaojin@163.com>
2020-10-23 22:28:26 +0800

    invalid string

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -150,6 +150,10 @@ static int lept_parse_string(lept_context *c, lept_value *v)
             c->top = head;
             return LEPT_PARSE_MISS_QUOTATION_MARK;
         default:
+            if((unsigned char)ch<0x20){
+                c->top=head;
+                return LEPT_PARSE_INVALID_STRING_CHAR;
+            }
             PUTC(c, ch);
         }
     }
